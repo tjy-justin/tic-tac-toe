8f91d7574ab6b96d1982c191777cb8a386047c8a
Justin Tan <raineedust@proton.me>
2023-04-03 18:43:59 +0800

    Add starting and win conditions

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 // Selectors
 
-// Modules
+// Player
 
-const gameBoard = () => {
-  const board = ["", "", "", "", "", "", "", "", ""];
+const createPlayer = (name, sign) => ({ name, sign });
+
+// gameBoard
+
+const flowCtrl = () => {
+  const playerOne = createPlayer("Player 1", "X");
+  const playerTwo = createPlayer("Player 2", "O");
+
+  // Game start
+  const activePlayer = playerOne;
+  const winner = false;
+  const gridSlots = 9;
+
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 };
 
-const displayController = () => {
-  const grid = document.querySelectorAll(".grid");
-};
-
-// Factories
-
-// flowCtrl obj
-
-// displayController module obj
+// Modules needed: gameBoard, displayController, flowController
+// Factories needed: players
