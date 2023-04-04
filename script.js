7242748573cd3de9305ac19582ba308b22a82b25
Justin Tan <raineedust@proton.me>
2023-04-04 16:37:49 +0800

    Implement grid interaction, winner check

--- script.js
+++ script.js
@@ -2,18 +2,55 @@
 
 // Player
 
-const createPlayer = (name, sign) => ({ name, sign });
-
 // gameBoard
 
-const flowCtrl = () => {
-  const playerOne = createPlayer("Player 1", "X");
-  const playerTwo = createPlayer("Player 2", "O");
+// Modules needed: gameBoard, displayController, flowController
+// Factories needed: players
+
+// What does a displayCtrl actually do?
+// 1. reduce gridslots 2. msg changes 3. reset button
+// 4. declare winner msg 5. add gridslots
+
+const createPlayer = (name, sign) => ({ name, sign });
+
+const gameBoard = (() => {
+  const board = [];
+  for (let i = 0; i < 9; i++) {
+    board.push("");
+  }
+
+  const grid = document.querySelector(".grid");
+
+  board.forEach((item, index) => {
+    const square = document.createElement("div");
+    square.className = "square";
+    grid.appendChild(square);
+  });
+
+  Array.from(grid.children).forEach((square, index) => {
+    square.addEventListener("click", () => {
+      square.classList.add(flowCtrl.activePlayer.sign);
+      square.setAttribute("data", flowCtrl.activePlayer.sign);
+      board[index] = flowCtrl.activePlayer.sign;
+      square.style.pointerEvents = "none";
+      flowCtrl.gridSlots -= 1;
+    });
+  });
+
+  return { board };
+})();
+
+const flowCtrl = (() => {
+  const playerOne = createPlayer("Player 1", "xmark");
+  const playerTwo = createPlayer("Player 2", "circle");
 
   // Game start
   const activePlayer = playerOne;
   const winner = false;
   const gridSlots = 9;
+
+  const msg = document.querySelector(".msg");
+  const playerName = document.querySelector(".playerName");
 
   const winConditions = [
     [0, 1, 2],
@@ -25,7 +62,25 @@ const flowCtrl = () => {
     [0, 4, 8],
     [2, 4, 6],
   ];
-};
 
-// Modules needed: gameBoard, displayController, flowController
-// Factories needed: players
+  function checkWinner() {
+    winConditions.forEach((item, index) => {
+      if (
+        gameBoard.board[item[0]] === this.activePlayer.sign &&
+        gameBoard.board[item[1]] === this.activePlayer.sign &&
+        gameBoard.board[item[2]] === this.activePlayer.sign
+      ) {
+        console.log("We have a winner!");
+        msg.textContent = `${this.activePlayer.name} wins!`;
+        this.winner = true;
+      }
+    });
+  }
+
+  return {
+    activePlayer,
+    gridSlots,
+    checkWinner,
+    winner,
+  };
+})();
